3dd1ac16e3bad0d5797b2db38a2a2b5f5154f065
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-07-12 15:50:03 +0530

    add last line

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ def caesar(original_text, shift_amount, encode_or_decode):
             shifted_position %= len(alphabet)
             output_text += alphabet[shifted_position]
     print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye!")
